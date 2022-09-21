80bc990dcaa5a240066f4efc6eb4477fe99f1cfd
Arthur Deruelle <deruelle.arthur@gmail.com>
2022-09-21 17:19:20 +0200

    get data from api and put dynañicly into the html

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const APIEndpoint = "http://api.quotable.io/random";
+const textToWrite = document.querySelector(".text-to-write")
+let ApiContent = {};
+const fetchSentence = async () => {
+  try {
+    let response = await fetch(APIEndpoint)
+    let result =  await response.json()
+    //console.log(result)
+    ApiContent.lengthApiSentence = result.length
+    ApiContent.ApiSentence = result.content
+    console.log("ApiContent", ApiContent)
+    displayContent(ApiContent)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const displayContent = (ApiContent) => {
+  textToWrite.textContent = ApiContent.ApiSentence;
+}
+fetchSentence()
