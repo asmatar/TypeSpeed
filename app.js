16664e0dc1c5b21b3d1d01a570586b26943d47a5
Arthur Deruelle <deruelle.arthur@gmail.com>
2022-09-22 00:22:19 +0200

    js part 1

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const APIEndpoint = "http://api.quotable.io/random";
 const textToWrite = document.querySelector(".text-to-write")
+const textToTest = document.querySelector(".text-to-test")
+const time = document.querySelector(".time")
+let currentScore = document.querySelector(".current-score")
+const timerDiv = document.querySelector(".timer")
+const scoreDiv = document.querySelector(".score")
 let ApiContent = {};
-const fetchSentence = async () => {
+let timer = 8
+let score = 0
+
+
+const getNewSentence = async () => {
   try {
     let response = await fetch(APIEndpoint)
     let result =  await response.json()
-    //console.log(result)
     ApiContent.lengthApiSentence = result.length
     ApiContent.ApiSentence = result.content
-    console.log("ApiContent", ApiContent)
     displayContent(ApiContent)
   } catch (error) {
     console.log(error)
@@ -18,4 +25,55 @@ const fetchSentence = async () => {
 const displayContent = (ApiContent) => {
   textToWrite.textContent = ApiContent.ApiSentence;
 }
-fetchSentence()
+const decreaseTime = () => {
+  time.innerText = timer
+  if (timer === 0) {
+    return
+  }
+  if (timer%2 == 0) {
+    timerDiv.style.backgroundImage= "linear-gradient(270deg, rgba(233,67,196,1) 0%, rgba(251,133,29,1) 100%)"
+  } else {
+    timerDiv.style.backgroundImage = "linear-gradient(270deg, rgba(143,233,67,1) 0%, rgba(29,251,222,1) 100%)"
+  }
+  timer--
+}
+
+getNewSentence()
+setInterval(decreaseTime, 1000)
+decreaseTime()
+
+const compareString = (event) => {
+  userText = event.target.value
+  userTextLength = event.target.value.length
+  //console.log("value.length",event.target.value.length )
+  console.log("textToTest.value", userText)
+
+  /* textToTest.length */
+  //console.log("text test length", ApiContent.lengthApiSentence)
+  testToVerify = ApiContent.ApiSentence.slice(0,userTextLength)
+  console.log("text test length", testToVerify)
+
+  if (userText === testToVerify) {
+  /*   console.log("textToWrite", textToWrite.textContent.slice(0, 2))
+    let colorText = textToWrite.textContent.slice(0, 2)
+    colorText.style.color = "red" */
+    if (timer === 0) {
+      return
+    }
+    currentScore.innerHTML = score++
+  } else {
+  }
+}
+
+const resetGame = (event) => {
+  if (event.keyCode === 27) {
+    timer = 10;
+    score = 0
+    currentScore.innerHTML = score;
+    textToTest.textContent = ""
+    textToTest.value = ""
+    getNewSentence()
+  }
+}
+window.addEventListener("keydown", resetGame)
+textToTest.addEventListener("input", compareString)
